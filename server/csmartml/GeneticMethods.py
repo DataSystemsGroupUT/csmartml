54fa12ed7f81fb949d35f5a0a70727d86fe79156
mjbahmani <mohamadjavad.bahmani@gmail.com>
2021-12-28 18:19:09 +0200

    refactor: use autopep8 for refactoring

--- server/csmartml/GeneticMethods.py
+++ server/csmartml/GeneticMethods.py
@@ -6,52 +6,54 @@ from .Algorithms import *
 
 class GeneticMethods:
 
-	def __init__(self, parameters, data, algorithm):
-		# Differentiate mutable params from immutable
-		self.params = parameters
-		self.param_size = len(parameters)
-		self.data = data
-		self.algorithm = algorithm
-		return
+    def __init__(self, parameters, data, algorithm):
+        # Differentiate mutable params from immutable
+        self.params = parameters
+        self.param_size = len(parameters)
+        self.data = data
+        self.algorithm = algorithm
+        return
 
-	def generate_pop(self):
-		pop = self.custom_algorithm_config(self.params)
-		return pop
+    def generate_pop(self):
+        pop = self.custom_algorithm_config(self.params)
+        return pop
 
-	def mutate(self, pop):
-		p = self.generate_pop()
-		tpop = copy.deepcopy(pop)
+    def mutate(self, pop):
+        p = self.generate_pop()
+        tpop = copy.deepcopy(pop)
 
-		for i in range(self.param_size):
-			pos = random.choice(list(range(0, self.param_size)))
-			setattr(tpop[0], self.params[pos], getattr(p, self.params[pos]))
+        for i in range(self.param_size):
+            pos = random.choice(list(range(0, self.param_size)))
+            setattr(tpop[0], self.params[pos], getattr(p, self.params[pos]))
 
-		return tpop,
+        return tpop,
 
-	def crossover(self, pop, pop2):
-		tpop, tpop2 = copy.deepcopy(pop), copy.deepcopy(pop2)
+    def crossover(self, pop, pop2):
+        tpop, tpop2 = copy.deepcopy(pop), copy.deepcopy(pop2)
 
-		for i in range(self.param_size):
-			pos = random.choice(list(range(0, self.param_size)))
-			setattr(tpop[0], self.params[pos], getattr(tpop2[0], self.params[pos]))
-			setattr(tpop2[0], self.params[pos], getattr(tpop[0], self.params[pos]))
+        for i in range(self.param_size):
+            pos = random.choice(list(range(0, self.param_size)))
+            setattr(tpop[0], self.params[pos],
+                    getattr(tpop2[0], self.params[pos]))
+            setattr(tpop2[0], self.params[pos],
+                    getattr(tpop[0], self.params[pos]))
 
-		return tpop, tpop2
+        return tpop, tpop2
 
-	def custom_algorithm_config(self, params):
+    def custom_algorithm_config(self, params):
 
-		algorithms = {
-			'kmeans': KMEANS(params, self.data),
-			'meanshift': MEANSHIFT(params, self.data),
-			'db': DB(params, self.data),
-			'ap': AP(params, self.data),
-			'spectral': Spectral(params, self.data),
-			'ag': AG(params, self.data),
-			'optics': OP(params, self.data),
-			'birch': BIRCH(params, self.data)
-		}
+        algorithms = {
+            'kmeans': KMEANS(params, self.data),
+            'meanshift': MEANSHIFT(params, self.data),
+            'db': DB(params, self.data),
+            'ap': AP(params, self.data),
+            'spectral': Spectral(params, self.data),
+            'ag': AG(params, self.data),
+            'optics': OP(params, self.data),
+            'birch': BIRCH(params, self.data)
+        }
 
-		return algorithms[self.algorithm].config()
+        return algorithms[self.algorithm].config()
 
 #
 # import pandas as pd
--- server/csmartml/MetaCVI.py
+++ server/csmartml/MetaCVI.py
@@ -1,15 +1,11 @@
+import pandas as pd
+from scipy.spatial.distance import cdist
+import csv
+import numpy as np
+from .metafeatures import Meta
 import sys
 sys.path.append('../')
-from .metafeatures import Meta
 
-import pandas as pd 
-import numpy as np
-import csv
-from ast import literal_eval
-from sklearn.preprocessing import MultiLabelBinarizer
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from scipy.spatial.distance import cdist
 
 class Meta_CVI:
 
@@ -63,7 +59,7 @@ class Meta_CVI:
 
         return [[cvi1, eval_labels[cvi1]], [cvi2, eval_labels[cvi2]], [cvi3, eval_labels[cvi3]]]
 
-    def search(self, algorithm ="kmeans"):
+    def search(self, algorithm="kmeans"):
 
         # 1 - Get other metafeatures from knowledge-base & their CVI combinations
         df_meta_db = pd.read_csv("./csmartml/metafeatures.csv")
@@ -79,19 +75,23 @@ class Meta_CVI:
         filename = "multi_" + algorithm
         combinations = []
         with open('./csmartml/new-pareto/' + filename + '.tsv') as tsvfile:
-            reader = csv.DictReader(tsvfile, delimiter='\t', fieldnames=['dataset', 'cvi', 'nmi'])
+            reader = csv.DictReader(tsvfile, delimiter='\t', fieldnames=[
+                                    'dataset', 'cvi', 'nmi'])
             for row in reader:
                 combinations.append(row)
 
         df_combinations = pd.DataFrame(combinations)
         df_combinations['nmi'] = df_combinations['nmi'].apply(pd.np.float64)
-        df_combinations['rank'] = df_combinations.groupby('dataset')['nmi'].rank(ascending=False, axis=0)
-        df_combinations = df_combinations.groupby('dataset').apply(lambda x: x.sort_values(['cvi']))
+        df_combinations['rank'] = df_combinations.groupby(
+            'dataset')['nmi'].rank(ascending=False, axis=0)
+        df_combinations = df_combinations.groupby(
+            'dataset').apply(lambda x: x.sort_values(['cvi']))
 
         # 3 - Compute Euclidean distance between instance and other metafeatures
         df_meta_db_val = df_meta_db.loc[:, df_meta_db.columns != 'dataset']
-        distance_matrix = cdist(df_meta_db_val, df_meta_db_val, metric = 'euclidean')
-        
+        distance_matrix = cdist(
+            df_meta_db_val, df_meta_db_val, metric='euclidean')
+
         instance_index = len(df_meta_db) - 1
         distances = np.trim_zeros(distance_matrix[instance_index])
         distances_sm = np.sort(distances)[0:5]
@@ -102,11 +102,13 @@ class Meta_CVI:
         for dist in distances_sm:
             index = np.where(distances == dist)
             ds = str(df_meta_db.iloc[index].dataset.values[0])
-            all_rank.append(df_combinations.loc[df_combinations['dataset'] == (ds)]['rank'].values)
+            all_rank.append(
+                df_combinations.loc[df_combinations['dataset'] == (ds)]['rank'].values)
 
             if len(all_cvis) == 0:
-                all_cvis = df_combinations.loc[df_combinations['dataset'] == (ds)]['cvi'].values
-        
+                all_cvis = df_combinations.loc[df_combinations['dataset'] == (
+                    ds)]['cvi'].values
+
         # 5 - Select and return best CVI combination by NMI Scores
         values = []
         for rn in all_rank:
@@ -115,7 +117,8 @@ class Meta_CVI:
 
         if len(values) > 0:
             fn_rank = np.mean(values, axis=0)       # Merged final rankings
-            top_rank_index = np.min(fn_rank)        # Get highest ranked CVI combo
+            # Get highest ranked CVI combo
+            top_rank_index = np.min(fn_rank)
             fn_cvi = all_cvis[np.where(fn_rank == top_rank_index)][0]
             return self.format_cvi(fn_cvi)
         else:
